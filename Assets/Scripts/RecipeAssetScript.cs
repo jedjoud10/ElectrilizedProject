2cadd022984f9f342d84e7e7001e7723de8a0258
Jedjoud10 <jribijed6@gmail.com>
2019-11-17 21:14:52 -0500

    Revert "Initial commit"
    This reverts commit 3fcd3f6c6a1c7b4015a935aea0161a62ffa7780d.

--- Assets/Scripts/RecipeAssetScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu()]
-public class RecipeAssetScript : ScriptableObject
-{
-    public ItemAssetScript[] inputs;
-    public ItemAssetScript output;
-}
